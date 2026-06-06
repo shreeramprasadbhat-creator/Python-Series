a8cd3b6cfc32cde8e746ddf62662df8f83842d04
shreeramprasad bhat <shreeramprasadbhat@gmail.com>
2026-06-06 10:16:38 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,11 @@
+text=(input("Enter the word: "))
+char=(input("Enter the char to remove it: "))
+result=" "
+remove=" "
+for i in text:
+    if i!=char:
+        result=result+i
+    else:
+        remove=remove+i
+print("updated text is: ",result)
+print('removed word is:',remove)
