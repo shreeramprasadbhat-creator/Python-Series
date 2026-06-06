bec8fb35bd2038b14672a16936a49ec601f17978
shreeramprasad bhat <shreeramprasadbhat@gmail.com>
2026-06-06 10:21:16 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,11 @@
+oval={'A','E','I','O','U','a','e','i','o','u'}
+text=input('enter the text:' )
+result=' '
+remove=' '
+for i in text:
+    if i not in oval:
+        result=result+i
+    else:
+        remove=remove+i
+print("updated text is:",result)
+print("removed text is:",remove)
